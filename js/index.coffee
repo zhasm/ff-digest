11368ecb068cea0eb27168505c8dd1bf96ca4840
Zhang Dongliang <zhangdongliang@meituan.com>
2015-10-05 22:08:27 +0800

    +json index

--- js/index.coffee
+++ js/index.coffee
@@ -1,51 +1,52 @@
 $ = jQuery
-_.templateSettings =   interpolate: /\{\{(.+?)\}\}/g
-
-tpl_img = """
-<span class="photo">
-<a href="{{ img.page }}" name="/photo/j8gBtn5zH3I" class="photo zoom" target="_blank">
-    <img src="{{ img.preview}}">
-</a>
-</span>
-"""
-
-tpl_msg = """
-  <tr>
-    <td class="item">
-      <div class="outer">
-        <div class="avatar">
-          <a href="http://fanfou.com/{{ loginname }}" title="{{ realname }}">
-            <img class="avatar" src="{{ avatar }}">
-          </a>
-        </div>
-        <div class="text-outer">
-          <span class="text">
-            <a href="http://fanfou.com/{{ loginname }}">{{ realname }}</a>
-                    {{ msg }}
-                </span>
-                {{ msghtml }}
-                <span class="signature">
-                    <a href="http://fanfou.com/statuses/{{ statusid }}" class="date">{{ time }}</a>
-                    <span class="fav">收藏 {{ count }} 次</span>
-                </span>
-        </div>
-      </div>
-    </td>
-  </tr>
-"""
 
 render_img = (item) ->
+
+  img = item.img
+
+  tpl_img = """
+  <span class="photo">
+  <a href="#{ img.page }" name="/photo/j8gBtn5zH3I" class="photo zoom" target="_blank">
+      <img src="#{ img.preview}">
+  </a>
+  </span>
+  """
+
   unless item.img.page?.length
     ''
   else
-    _.template(tpl_img)(item)
+    tpl_img
 
 render_item = (item) ->
   item.msghtml = render_img item
-  _.template(tpl_msg)(item)
+
+  """
+    <tr>
+      <td class="item">
+        <div class="outer">
+          <div class="avatar">
+            <a href="http://fanfou.com/#{ item.loginname }" title="#{ item.realname }">
+              <img class="avatar" src="#{ item.avatar }">
+            </a>
+          </div>
+          <div class="text-outer">
+            <span class="text">
+              <a href="http://fanfou.com/#{ item.loginname }">#{ item.realname }</a>
+                      #{ item.msg }
+                  </span>
+                  #{ item.msghtml }
+                  <span class="signature">
+                      <a href="http://fanfou.com/statuses/#{ item.statusid }" class="date">#{ item.time }</a>
+                      <span class="fav">收藏 #{ item.count } 次</span>
+                  </span>
+          </div>
+        </div>
+      </td>
+    </tr>
+  """
 
 render_page = (data) ->
-  title = _.template("饭否{{ shift_cn }}精选 {{date}}")(data)
+  title = "饭否#{ data.shift_cn }精选 #{ data.date }"
   $('title, h2.content-head').html title
 
   tophr = '''<tr>
@@ -57,7 +58,24 @@ render_page = (data) ->
   $('.content tbody').prepend items
   $('.content tbody').prepend tophr
 
+routing = () ->
+  hash = window.location.hash
+
+  get_page = (url) ->
+    $.get url, (data) ->
+      render_page data
+
+  #default routing: use the top one
+  unless hash?.length
+    index = './json/index.json'
+    $.get index, (data)->
+      get_page(data[0])
+
+  url_pattern = ///\d{4}-\d{2}-\d{2}\.(?:daily|weekly)///
+  if hash.match url_pattern
+    fn = hash.match(url_pattern)[0]
+    url = "./json/#{fn}.json"
+    get_page url
+
 $ () ->
-  f = './json/2015-10-05.daily.json'
-  $.get f, (data)->
-    render_page data
+  routing()
--- js/index.js
+++ js/index.js
@@ -1,33 +1,27 @@
 (function() {
-  var $, render_img, render_item, render_page, tpl_img, tpl_msg;
+  var $, render_img, render_item, render_page, routing;
 
   $ = jQuery;
 
-  _.templateSettings = {
-    interpolate: /\{\{(.+?)\}\}/g
-  };
-
-  tpl_img = "<span class=\"photo\">\n<a href=\"{{ img.page }}\" name=\"/photo/j8gBtn5zH3I\" class=\"photo zoom\" target=\"_blank\">\n    <img src=\"{{ img.preview}}\">\n</a>\n</span>";
-
-  tpl_msg = "<tr>\n  <td class=\"item\">\n    <div class=\"outer\">\n      <div class=\"avatar\">\n        <a href=\"http://fanfou.com/{{ loginname }}\" title=\"{{ realname }}\">\n          <img class=\"avatar\" src=\"{{ avatar }}\">\n        </a>\n      </div>\n      <div class=\"text-outer\">\n        <span class=\"text\">\n          <a href=\"http://fanfou.com/{{ loginname }}\">{{ realname }}</a>\n                  {{ msg }}\n              </span>\n              {{ msghtml }}\n              <span class=\"signature\">\n                  <a href=\"http://fanfou.com/statuses/{{ statusid }}\" class=\"date\">{{ time }}</a>\n                  <span class=\"fav\">收藏 {{ count }} 次</span>\n              </span>\n      </div>\n    </div>\n  </td>\n</tr>";
-
   render_img = function(item) {
-    var _ref;
+    var img, tpl_img, _ref;
+    img = item.img;
+    tpl_img = "<span class=\"photo\">\n<a href=\"" + img.page + "\" name=\"/photo/j8gBtn5zH3I\" class=\"photo zoom\" target=\"_blank\">\n    <img src=\"" + img.preview + "\">\n</a>\n</span>";
     if (!((_ref = item.img.page) != null ? _ref.length : void 0)) {
       return '';
     } else {
-      return _.template(tpl_img)(item);
+      return tpl_img;
     }
   };
 
   render_item = function(item) {
     item.msghtml = render_img(item);
-    return _.template(tpl_msg)(item);
+    return "<tr>\n  <td class=\"item\">\n    <div class=\"outer\">\n      <div class=\"avatar\">\n        <a href=\"http://fanfou.com/" + item.loginname + "\" title=\"" + item.realname + "\">\n          <img class=\"avatar\" src=\"" + item.avatar + "\">\n        </a>\n      </div>\n      <div class=\"text-outer\">\n        <span class=\"text\">\n          <a href=\"http://fanfou.com/" + item.loginname + "\">" + item.realname + "</a>\n                  " + item.msg + "\n              </span>\n              " + item.msghtml + "\n              <span class=\"signature\">\n                  <a href=\"http://fanfou.com/statuses/" + item.statusid + "\" class=\"date\">" + item.time + "</a>\n                  <span class=\"fav\">收藏 " + item.count + " 次</span>\n              </span>\n      </div>\n    </div>\n  </td>\n</tr>";
   };
 
   render_page = function(data) {
     var item, items, title, tophr;
-    title = _.template("饭否{{ shift_cn }}精选 {{date}}")(data);
+    title = "饭否" + data.shift_cn + "精选 " + data.date;
     $('title, h2.content-head').html(title);
     tophr = '<tr>\n  <td id="top-hr"></td>\n</tr>';
     items = (function() {
@@ -44,12 +38,30 @@
     return $('.content tbody').prepend(tophr);
   };
 
+  routing = function() {
+    var fn, get_page, hash, index, url, url_pattern;
+    hash = window.location.hash;
+    get_page = function(url) {
+      return $.get(url, function(data) {
+        return render_page(data);
+      });
+    };
+    if (!(hash != null ? hash.length : void 0)) {
+      index = './json/index.json';
+      $.get(index, function(data) {
+        return get_page(data[0]);
+      });
+    }
+    url_pattern = /\d{4}-\d{2}-\d{2}\.(?:daily|weekly)/;
+    if (hash.match(url_pattern)) {
+      fn = hash.match(url_pattern)[0];
+      url = "./json/" + fn + ".json";
+      return get_page(url);
+    }
+  };
+
   $(function() {
-    var f;
-    f = './json/2015-10-05.daily.json';
-    return $.get(f, function(data) {
-      return render_page(data);
-    });
+    return routing();
   });
 
 }).call(this);
